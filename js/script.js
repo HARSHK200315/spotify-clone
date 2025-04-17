374f9e5c737e51dabf53e171899019d4a2da1b3e
HARSHK200315 <harshkocharekar2003@gmail.com>
2025-04-17 17:44:27 +0530

    fix songs not loading

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 let currentSong = new Audio();
-let songs;
-let currFolder;
+let songs = [];
+let currFolder = "";
 
 function secondsToMinutesSeconds(seconds) {
     if (isNaN(seconds) || seconds < 0) {
@@ -18,105 +18,89 @@ function secondsToMinutesSeconds(seconds) {
 
 async function getSongs(folder) {
     currFolder = folder;
-    let a = await fetch(`/songs/${folder}/info.json`)
-    let response = await a.json();
-    let div = document.createElement("div")
-    div.innerHTML = response;
-    let as = div.getElementsByTagName("a")
-    songs = response.songs;
-    for (let index = 0; index < as.length; index++) {
-        const element = as[index];
-        if (element.href.endsWith(".mp3")) {
-            songs.push(element.href.split(`/songs/${folder}/`)[1])
-        }
-    }
- 
-
-
-    // Show all the songs in the playlist
-    let songUL = document.querySelector(".songList").getElementsByTagName("ul")[0]
-    songUL.innerHTML = ""
+    let response  = await fetch(`/${folder}/info.json`)
+    let data  = await response.json();
+    songs = data.songs;
+    
+    let songUL = document.querySelector(".songList ul");
+    songUL.innerHTML = "";
     for (const song of songs) {
-        songUL.innerHTML = songUL.innerHTML + `<li><img class="invert" width="34" src="img/music.svg" alt="">
-                            <div class="info">
-                                <div> ${song.replaceAll("%20", " ")}</div>
-                                <div>Harry</div>
-                            </div>
-                            <div class="playnow">
-                                <span>Play Now</span>
-                                <img class="invert" src="img/play.svg" alt="">
-                            </div> </li>`;
+        songUL.innerHTML += `
+            <li>
+                <img class="invert" width="34" src="/img/music.svg" alt="">
+                <div class="info">
+                    <div>${song.replaceAll("%20", " ")}</div>
+                    <div>Artist</div>
+                </div>
+                <div class="playnow">
+                    <span>Play Now</span>
+                    <img class="invert" src="/img/play.svg" alt="">
+                </div>
+            </li>`;
     }
 
     // Attach an event listener to each song
-    Array.from(document.querySelector(".songList").getElementsByTagName("li")).forEach(e => {
-        e.addEventListener("click", element => {
-            playMusic(e.querySelector(".info").firstElementChild.innerHTML.trim())
-
-        })
-    })
+    Array.from(document.querySelectorAll(".songList li")).forEach(e => {
+        e.addEventListener("click", () => {
+            const track = e.querySelector(".info div").innerText.trim();
+            playMusic(track);
+        });
+    });
 
     return songs
 }
 
 const playMusic = (track, pause = false) => {
-    currentSong.src = `/${currFolder}/` + track
+    currentSong.src = `/${currFolder}/${track}`;
     if (!pause) {
         currentSong.play()
         play.src = "img/pause.svg"
     }
     document.querySelector(".songinfo").innerHTML = decodeURI(track)
     document.querySelector(".songtime").innerHTML = "00:00 / 00:00"
-
-
 }
 
-async function displayAlbums() {
-    console.log("displaying albums")
-    let a = await fetch(`/songs/`)
-    let response = await a.text();
-    let div = document.createElement("div")
-    div.innerHTML = response;
-    let anchors = div.getElementsByTagName("a")
-    let cardContainer = document.querySelector(".cardContainer")
-    let array = Array.from(anchors)
-    for (let index = 0; index < array.length; index++) {
-        const e = array[index]; 
-        if (e.href.includes("/songs") && !e.href.includes(".htaccess")) {
-            let folder = e.href.split("/").slice(-2)[0]
-            // Get the metadata of the folder
-            let a = await fetch(`/songs/${folder}/info.json`)
-            let response = await a.json(); 
-            cardContainer.innerHTML = cardContainer.innerHTML + ` <div data-folder="${folder}" class="card">
-            <div class="play">
-                <svg width="16" height="16" viewBox="0 0 24 24" fill="none"
-                    xmlns="http://www.w3.org/2000/svg">
-                    <path d="M5 20V4L19 12L5 20Z" stroke="#141B34" fill="#000" stroke-width="1.5"
-                        stroke-linejoin="round" />
-                </svg>
-            </div>
+// Load albums from /songs folder
+async function displayAlbums(folder) {
+    console.log("Fetching albums metadata...");
+    let response = await fetch(`/songs/playlist.json`);
+    let albums = await response.json();
 
-            <img src="/songs/${folder}/cover.jpeg" alt="">
-            <h2>${response.title}</h2>
-            <p>${response.description}</p>
-        </div>`
-        }
+    let cardContainer = document.querySelector(".cardContainer");
+    cardContainer.innerHTML = ""; // Clear existing content
+
+    for (let album of albums) {
+        cardContainer.innerHTML += `
+            <div data-folder="${album}" class="card">
+                <div class="play">
+                    <svg width="16" height="16" viewBox="0 0 24 24" fill="none"
+                        xmlns="http://www.w3.org/2000/svg">
+                        <path d="M5 20V4L19 12L5 20Z" stroke="#141B34" fill="#000" stroke-width="1.5"
+                            stroke-linejoin="round" />
+                    </svg>
+                </div>
+                <img src="/songs/${album}/cover.jpeg" alt="">
+                <h2>${album.title}</h2>
+                <p>${album.description}</p>
+            </div>`;
+
+            console.log("Loaded albums:", albums);
+
     }
 
-    // Load the playlist whenever card is clicked
-    Array.from(document.getElementsByClassName("card")).forEach(e => { 
-        e.addEventListener("click", async item => {
-            console.log("Fetching Songs")
-            songs = await getSongs(`/songs/${item.currentTarget.dataset.folder}`)  
-            playMusic(songs[0])
-
-        })
-    })
+    // Load the playlist when a card is clicked
+    document.querySelectorAll(".card").forEach(card => {
+        card.addEventListener("click", async () => {
+            songs = await getSongs(`songs/${card.dataset.folder}`);
+            playMusic(songs[0]);
+        });
+    });
 }
+
 
 async function main() {
     // Get the list of all the songs
-    await getSongs("/songs/ncs")
+    await getSongs("songs/ncs")
     playMusic(songs[0], true)
 
     // Display all the albums on the page
@@ -162,8 +146,8 @@ async function main() {
     previous.addEventListener("click", () => {
         currentSong.pause()
         console.log("Previous clicked")
-        let index = songs.indexOf(currentSong.src.split("/").slice(-1)[0])
-        if ((index - 1) >= 0) {
+        let index = songs.indexOf(currentSong.src.split("/").pop())
+        if ((index) > 0) {
             playMusic(songs[index - 1])
         }
     })
@@ -173,15 +157,15 @@ async function main() {
         currentSong.pause()
         console.log("Next clicked")
 
-        let index = songs.indexOf(currentSong.src.split("/").slice(-1)[0])
-        if ((index + 1) < songs.length) {
+        let index = songs.indexOf(currentSong.src.split("/").pop())
+        if ((index) < songs.length) {
             playMusic(songs[index + 1])
         }
     })
 
     // Add an event to volume - add input insted of change to make it live volume changer
-    document.querySelector(".range").getElementsByTagName("input")[0].addEventListener("input", (e) => {
-        console.log("Setting volume to", e.target.value, "/ 100")
+    document.querySelector(".range input").addEventListener("input", (e) => {
+        // console.log("Setting volume to", e.target.value, "/ 100")
         currentSong.volume = parseInt(e.target.value) / 100
         if (currentSong.volume >0){
             document.querySelector(".volume>img").src = document.querySelector(".volume>img").src.replace("mute.svg", "volume.svg")
